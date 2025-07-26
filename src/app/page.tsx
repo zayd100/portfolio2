58b7d45e8000af3c0a7198392a8b43daaaacc303
zaid <zaidshabbir666@gmail.com>
2025-07-25 19:58:48 -0700

    txT

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -85,7 +85,7 @@ export default function Page() {
           delay={BLUR_FADE_DELAY}
           className="text-2xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none"
           yOffset={8}
-          text={`${DATA.name.split(" ")[0]} Chaudhary `}
+          text={`Mian ${DATA.name.split(" ")[0]} Chaudhary `}
         />
         <BlurFadeText
           className="max-w-[700px] md:text-xl text-muted-foreground"
