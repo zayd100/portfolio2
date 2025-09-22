02c34766e010f17d9232bb00276a8f4dbdad3ea7
zayd100 <135010678+zayd100@users.noreply.github.com>
2025-09-22 11:52:13 -0700

    Update resume.tsx

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -54,7 +54,12 @@ Technical Writer & Researcher`,
 
         navbar: true,
       },
-    
+              name: "LinkedIn",
+        url: "https://www.linkedin.com/in/zaid-shabbir-953b4b184?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+        icon: Icons.linkedin,
+
+        navbar: true,
+      },
       X: {
         name: "X",
         url: "https://x.com/sharpzayd",
