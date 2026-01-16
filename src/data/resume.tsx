61f6c48340b14c08d6b6598e953ee34cebe71386
zayd100 <135010678+zayd100@users.noreply.github.com>
2026-01-16 05:06:32 +0500

    Update resume.tsx

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -12,7 +12,7 @@ export const DATA = {
   locationLink: "https://www.google.com/maps/place/karachi",
 description: `Full Stack Engineer`,
   summary:
-  "I'm a full-stack developer from Pakistan with 2 years building production apps, working with US-based entrepreneurs and startups.I've built a strong foundation in modern web technologies while delivering high-quality solutions for Businesses around the world. My work extends beyond development to include comprehensive research in emerging technologies—particularly blockchain applications, spatial audio systems, and decentralized identity solutions using structured knowledge management systems. Currently freelancing and developing my own projects, I'm passionate about turning innovative ideas into robust digital products through both technical implementation and thorough research. Always open to new opportunities, collaborations, and exciting challenges.",
+  "I'm a full-stack developer from Pakistan with 2 years building production apps, working with US-based entrepreneurs and startups.I've built a strong foundation in modern web technologies while delivering high-quality solutions for Businesses around the world. My work extends beyond development to include comprehensive research in emerging technologies, particularly blockchain applications, spatial audio systems, and decentralized identity solutions using structured knowledge management systems. Currently freelancing and developing my own projects, I'm passionate about turning innovative ideas into robust digital products through both technical implementation and thorough research. Always open to new opportunities, collaborations, and exciting challenges.",
 
   avatarUrl: "/hi.png",
   skills: [
